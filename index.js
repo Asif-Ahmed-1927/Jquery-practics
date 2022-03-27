8b446463f4c9d2b8f07e870a06d4e32ead5d3c56
Asif Ahmed <asifbin8@gmail.com>
2022-03-27 12:01:28 +0600

    commit

--- index.js
+++ index.js
@@ -44,24 +44,78 @@
 
 //login button eventLisetner
 
-$("#loginButton").click(function(){
+// $("#loginButton").click(function(){
 
-    var password1= $("#pass1").val();
-    var password2= $("#pass2").val();
+//     var password1= $("#pass1").val();
+//     var password2= $("#pass2").val();
 
-    if(password1 != "" && password2 != ""){
+//     if(password1 != "" && password2 != ""){
 
-        if(password1 == password2){
-            alert("successfully login")
-        } else{
-            alert("password mismatch")
-        }
+//         if(password1 == password2){
+//             alert("successfully login")
+//         } else{
+//             alert("password mismatch")
+//         }
 
-    }else{
-        alert("please enter password")
-    }
+//     }else{
+//         alert("please enter password")
+//     }
     
-})
+// })
 
+//animation
+
+// $("#btn").click(function(){
+//     $("#div1").hide(1000).show(2000);
+// })
+
+// $("#btn").click(function(){
+//     $("#div1").toggle(3000);
+// })
+
+// $("#btn").click(function(){
+//     $("#div1").fadeOut(3000);
+// })
+
+// $("#btn").click(function(){
+//     $("#div1").fadeIn(3000);
+// })
+ 
+// $("#btn").click(function(){
+//     $("#div1").fadeToggle(3000);
+// })
+ 
+// $("#btn").click(function(){
+//     $("#div1").fadeTo(2000, 0.3);
+// })
+ 
+// $("#btn").click(function(){
+//     $("#div1").slideUp(3000);
+// })
+
+// $("#btn").click(function(){
+//     $("#div1").slideDown(2000);
+// });
+
+// $("#btn").click(function(){
+//     $("#div1").slideToggle(2000);
+// });
+
+//animate method
+
+// $("#btn").click(function(){
+//     $("#div1").animate({opacity:"0.5"},2000);
+// })
+
+$("#btn").click(function(){
+    $("#div1").animate(
+        {
+
+            height:"400px", 
+            width:"900px",
+            marginLeft: "30px"
+
+        },2000);
+})
  
 
